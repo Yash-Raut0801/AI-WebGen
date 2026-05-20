684d44b0733c270e32d594d43ecc5bf1f6c50859
Yash Raut <yashraut617@gmail.com>
2026-05-20 19:50:04 +0530

    Installed mongoose

--- server/projects/project-1774125476024/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProjects, createProject } = require('../controllers/projectController');
-const router = express.Router();
-
-router.route('/')
-  .get(getProjects)
-  .post(createProject); // For demonstration, you might want to protect this in a real app
-
-module.exports = router;
